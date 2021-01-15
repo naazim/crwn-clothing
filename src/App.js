5ac6d076d8f77bd16bf02b2d3d5417a24a61b70e
Naasim Shareef <naasim.shareef@diconium.com>
2021-01-15 18:13:13 +0100

    Implemented firebase utils Add registration

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import "./App.css";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       currentUser: null
@@ -17,9 +17,21 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log("%c RENDERED_USER", "background: #029640; color: #fff", user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,11 +5,45 @@ import "firebase/auth";
 const config = {
   apiKey: "AIzaSyBTM52pdHF8FXVI07aUWJsgXxhXDwuTS3k",
   authDomain: "crwn-db-3d24b.firebaseapp.com",
+  databaseURL:
+    "https://crwn-db-3d24b-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "crwn-db-3d24b",
   storageBucket: "crwn-db-3d24b.appspot.com",
   messagingSenderId: "506011488422",
   appId: "1:506011488422:web:88555ba3e7e8c60b4ce467",
   measurementId: "G-PYYH01XNG0"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  console.log(
+    "%c RENDERED ",
+    "background: #029640; color: #fff",
+    userRef,
+    snapshot
+  );
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
