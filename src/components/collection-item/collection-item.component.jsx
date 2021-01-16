427e50e04d4432e508817c8223fe26d560601fb7
Naasim Shareef <naasim.shareef@diconium.com>
2021-01-16 13:14:24 +0100

    Toggle Cart visibility

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({ name, price, imageUrl }) => {
   return (
@@ -9,6 +10,7 @@ const CollectionItem = ({ name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
