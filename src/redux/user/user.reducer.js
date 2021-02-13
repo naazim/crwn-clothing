172650b4debb98cf4a6a17ef1096179f322a4acf
Naasim Shareef <naasim.shareef@diconium.com>
2021-02-13 09:44:40 +0100

    Move sign up to Sagas

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -7,6 +7,12 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case UserActionTypes.SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        currentUser: action.payload,
+        error: null
+      };
     case UserActionTypes.SIGN_IN_SUCCESS:
       return {
         ...state,
@@ -21,6 +27,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
       };
     case UserActionTypes.SIGN_IN_FAILURE:
     case UserActionTypes.SIGN_OUT_FAILURE:
+    case UserActionTypes.SIGN_UP_FAILURE:
       return {
         ...state,
         error: action.payload
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -10,12 +10,18 @@ import {
   signInFailure,
   signInSuccess,
   signOutFailure,
-  signOutSuccess
+  signOutSuccess,
+  signupFailure,
+  signUpSuccess
 } from "./user.actions";
 
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth, additionalData) {
   try {
-    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userRef = yield call(
+      createUserProfileDocument,
+      userAuth,
+      additionalData
+    );
     const userSnapshot = yield userRef.get();
     yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
   } catch (error) {
@@ -38,6 +44,21 @@ export function* signInWithEmail({ payload: { email, password } }) {
     yield getSnapshotFromUserAuth(user);
   } catch (error) {
     yield put(signInFailure(error));
+  }
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalData } }) {
+  yield getSnapshotFromUserAuth(user, additionalData);
+}
+
+export function* signUp({ payload: { email, password, displayName } }) {
+  try {
+    const { user } = yield auth.createUserWithEmailAndPassword(email, password);
+    // const userRef = yield createUserProfileDocument(user, { displayName });
+    // const userSnapshot = yield userRef.get();
+    yield put(signUpSuccess({ user, additionalData: { displayName } }));
+  } catch (error) {
+    yield put(signupFailure(error));
   }
 }
 
@@ -76,11 +97,20 @@ export function* onSignOutStart() {
   yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
 }
 
+export function* onSignUpStart() {
+  yield takeLatest(UserActionTypes.SIGN_UP_START, signUp);
+}
+export function* onSignUpSuccess() {
+  yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
     call(onCheckUserSession),
-    call(onSignOutStart)
+    call(onSignOutStart),
+    call(onSignUpStart),
+    call(onSignUpSuccess)
   ]);
 }
