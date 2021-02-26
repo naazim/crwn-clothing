fd880dee36ae3842814c6ccb7c48ea77dca14b79
Naasim Shareef <naasim.shareef@diconium.com>
2021-02-26 16:17:16 +0100

    Remove unused cors

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 
@@ -10,11 +9,10 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log('======PORT======= ', port)
+console.log("======PORT======= ", port);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
